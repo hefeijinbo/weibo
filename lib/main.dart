cf90ebabe4a912f9625b438d5342121319cfcee9
bowen.jin <bowen.jin@zoom.us>
2022-06-01 10:51:43 +0800

    support leancloud sdk

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:weibo/pages/splash_page.dart';
 import 'package:weibo/public.dart';
+import 'package:leancloud_storage/leancloud.dart';
 
 void main() {
   runApp(new MyApp());
+  LeanCloud.initialize(
+      "p3YYULf1m77fkNeJxhWFnFzo-9Nh9j0Va", "HcL2CSSGcShlibiem0QnCGoq",
+      server: "https://p3yyulf1.lc-cn-e1-shared.com",
+      queryCache: new LCQueryCache());
   if (Platform.isAndroid) {
       SystemUiOverlayStyle systemUiOverlayStyle = SystemUiOverlayStyle(
       systemNavigationBarColor: Color(0xffffffff),
